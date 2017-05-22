f08753280ab4affff62f7bddfc52a0d42c537f36
LG Laptop <devin@LG.Laptop>
2017-05-22 20:20:31 +0900

    Basic Toolbar setting.

--- app/src/main/java/com/example/devin/noactionbartest/MainActivity.java
+++ app/src/main/java/com/example/devin/noactionbartest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.devin.noactionbartest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
 }
